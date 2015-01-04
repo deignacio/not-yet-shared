d409af28d2ed3f8d42ac6a59742715ff047d2ba1
David Ignacio <deignacio@gmail.com>
2015-01-03 20:33:49 -0800

    add PlayerState to the Player.playTurn signature

--- src/org/ignacios/notyet/player/Player.java
+++ src/org/ignacios/notyet/player/Player.java
@@ -1,6 +1,7 @@
 package org.ignacios.notyet.player;
 
 import org.ignacios.notyet.state.GameState;
+import org.ignacios.notyet.state.PlayerState;
 
 /**
  * Interface that all bots must implement.
@@ -10,7 +11,8 @@ public interface Player {
     /**
      * Performs the player's turn.
      * @param gameState the current state of the state
+     * @param playerState the state of the current player
      * @return the {@link Action} to perform
      */
-    Action playTurn(GameState gameState);
+    Action playTurn(GameState gameState, PlayerState playerState);
 }
