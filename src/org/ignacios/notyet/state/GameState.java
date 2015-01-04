d97ecc60eae489a798aa8abd1dbb62a17ddd3750
David Ignacio <deignacio@gmail.com>
2015-01-03 21:10:28 -0800

    add visibility into cards left and cards taken to the GameState

--- src/org/ignacios/notyet/state/GameState.java
+++ src/org/ignacios/notyet/state/GameState.java
@@ -5,6 +5,7 @@ import org.ignacios.notyet.card.Card;
 import org.ignacios.notyet.turn.Turn;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Class containing the state of a game.
@@ -15,6 +16,16 @@ public interface GameState {
      * @return the initial game state
      */
     InitialState getInitialState();
+
+    /**
+     * @return the number of cards left in the deck
+     */
+    int getNumberOfCardsLeft();
+
+    /**
+     * @return the cards already seen in the game, does not include the current card
+     */
+    Set<Card> getCardsTaken();
 
     /**
      * @return the card currently up for bid
